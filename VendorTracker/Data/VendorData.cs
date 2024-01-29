4a9c9a214b6371c4a2817dcf3384f494224dfdcf
gabe <gabrieltucker260@gmail.com>
2024-01-28 22:13:45 -0800

    added data file to put list

---
+++ VendorTracker/Data/VendorData.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using VendorTracker.Models;
+
+namespace VendorTracker.Data
+{
+    public static class VendorData
+    {
+        public static List<Vendor> Vendors { get; } = new List<Vendor>();
+    }
+}
